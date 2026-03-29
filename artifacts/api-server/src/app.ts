115b9755542716ef0cbad2044e699cba8e114917
nihadaboudou-max <nihad.aboudou@ism.edu.sn>
2026-03-29 23:53:00 +0000

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -25,10 +25,14 @@ app.use(
     },
   }),
 );
-app.use(cors());
+
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", router);
 
 export default app;
